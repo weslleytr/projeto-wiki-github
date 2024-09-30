673662219ac39e7312f3fb01786dd21381861ac2
weslleytr <weslleyatr07@gmail.com>
2024-09-30 14:41:17 -0300

    first commit

---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div `
+    width: 80%;
+    height: auto;
+
+    h3 {
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+
+    p {
+        font-size: 16px;
+        color: #FAFAFA60;
+        margin-bottom: 20px;
+    }
+
+    a {
+        color: #87CEFA;
+        margin-top: 20px;
+        text-decoration: none;
+    }
+
+    a.remover {
+        color: #E83b31;
+        margin-top: 20px;
+        text-decoration: none;
+    }
+
+    hr {
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+`
